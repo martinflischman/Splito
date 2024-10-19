425c458dd30fd04acd00f63404e63672060d3c9c
Martin Flischman <martincodes@icloud.com>
2024-10-19 19:00:51 +0200

    Initial Commit

---
+++ Splito/SplitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitoApp.swift
+//  Splito
+//
+//  Created by Martin on 2024/10/19.
+//
+
+import SwiftUI
+
+@main
+struct SplitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
